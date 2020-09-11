49f8a5b1d46b926ebce1fc37accd645dffbaaa32
Heo Jong Tae <heo9910@naver.com>
2020-09-12 00:00:45 +0900

    .

--- netty-study-03-pipeline/src/main/java/co/kr/hjt/bootstrap/ChannelInboundEventHandler.java
+++ netty-study-03-pipeline/src/main/java/co/kr/hjt/bootstrap/ChannelInboundEventHandler.java
@@ -7,29 +7,35 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
- * 클라이언트로 부터 받은 데이터를 처리
+ * ChannelInbound 이벤트는 소켓 채널에 발생한 이벤트 중에서 연결 상대방이 어떤 동작을 취했을 때 발생
  * @author USER
  *
  */
-public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+public class ChannelInboundEventHandler extends ChannelInboundHandlerAdapter {
 
 	/**
-	 * 클라이언트로 부터 데이터 수신시 호출
-	 * @param ChannelHandlerContext ctx - 채널 파이프라인에 대한 이벤트처리
-	 * @param Object msg - 수신된 데이터
+	 * 데이터 수신시 호출 - 채널에 읽을 데이터가 있을 때 발생
 	 */
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		String readMessage = ((ByteBuf)msg).toString(Charset.defaultCharset());
 		System.out.println("수신한 문자열 [" + readMessage + "]");
 		ctx.write(msg); //서버에 연결된 클라이언트 채널로 입력받은 데이터를 전송
+		//ctx.writeAndFlush(msg); //데이터를 받자 마자 상대방에게 전송하게 됨
 	}
 
 	/**
-	 * channelRead 이벤트 처리가 완료된 후 자동으로 수행되는 이벤트 메서드
+	 * 데이터 수신이 완료되었음 - 채널의 데이터를 다 읽어서 더이상 데이터가 없을때 발생
 	 */
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		ctx.flush(); //채널 파이프라인에 저장된 버퍼를 전송
+	}
+	
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
 	}
 }
--- netty-study-03-pipeline/src/main/java/co/kr/hjt/bootstrap/EchoServer.java
+++ netty-study-03-pipeline/src/main/java/co/kr/hjt/bootstrap/EchoServer.java
@@ -9,16 +9,9 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-/**
- * 논블로킹 입출력 모드를 지원하는 ServerBootStrap 초기화
- */
 public class EchoServer {
 	public static void main(String[] args) throws Exception {
-		
-		//클라이언트 연결 수락 스레드 그룹 - 생성자 파라미터에 입력된 스레드 수가 1이므로 단일 스레드로 동작
 		EventLoopGroup bossGroup = new NioEventLoopGroup(1); 
-		
-		//연결된 클라이언트 소켓으로부터 데이터 입출력 및 이벤트를 처리를 담당하는 자식 스레드 그룹 - CPU코어수에 따른 스레드 수가 설정
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		
 		try {
@@ -28,18 +21,16 @@ public class EchoServer {
 			ServerBootstrap b = new ServerBootstrap();
 			b.group(bossGroup, workerGroup)
 			 .channel(NioServerSocketChannel.class) //서버소켓 네트워크 입출력 모드를 NIO 모드로 설정
-			 
-			 //자식 채널 초기화 방법 설정
+			  //연결된 클라이언트 소켓이 사용할 채널 파이프라인을 설정
 			 .childHandler(new ChannelInitializer<SocketChannel>() { 
-				//ChannelInitializer는 클라이언트로부터 연결된 채널이 초기화 될때의 기본동작이 지정된 추상클래스
+				//initChannel은 클라이언트 소켓이 생성될때 자동으로 호출
 				@Override
 				protected void initChannel(SocketChannel ch) throws Exception {
 					ChannelPipeline p = ch.pipeline(); //채널 파이프라인 객체 생성
-					p.addLast(new EchoServerHandler());//채널파이프라인에 클라이언트 연결시 데이터처리할 이벤트 핸들러 등록
+					p.addLast(new ChannelInboundEventHandler());//채널파이프라인에 클라이언트 연결시 데이터처리할 이벤트 핸들러 등록
 				}
 			});
 			
-			//ServerBootstrap.bind를 이용하여 접속할 포트지정
 			ChannelFuture f = b.bind(8888).sync();
 			f.channel().closeFuture().sync();
 		} finally {
